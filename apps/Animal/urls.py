b5667a7a5c52f41833735b0b5a79d538d08e0a46
JORGE LUIS SILVA CORREA <jsilvac@quala.com.co>
2020-04-20 16:22:57 -0500

    CRUD Animales

--- apps/Animal/urls.py
+++ apps/Animal/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 #from django.contrib import admin
 #from django.urls import path
-from apps.Animal.views import index
+from apps.Animal.views import index, animal_form, animal_list, animal_edit, animal_delete
 
 urlpatterns = [
-    url(r'^index$', index),
+    url(r'^$', index),
+    url(r'^nuevo$', animal_form),
+    url(r'^listar$', animal_list),
+    url(r'^editar/(?P<id_animal>\d+)/$', animal_edit),
+    url(r'^eliminar/(?P<id_animal>\d+)/$', animal_delete),
 
 ]
